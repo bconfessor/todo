be02212781a7795d3824c81f6c9649198fe1cc38
Brian Confessor <brian_confessor@poli.ufrj.br>
2023-01-11 12:40:26 +0900

    Tried to add div title to main page I guess

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -2,6 +2,7 @@ function App() {
     const { Container, Row, Col } = ReactBootstrap;
     return (
         <Container>
+            <div> <h1> ToDo App by Brian Confessor </h1> </div>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
                     <TodoListCard />
